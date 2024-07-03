e5c81c70af57fe345e87b35f4edeed393ac9d723
Zelyna Farrell <zsillas@yahoo.com>
2024-07-03 14:24:58 -0700

    Initial Commit

---
+++ ProfilePictureHeroAnimation/ProfilePictureHeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfilePictureHeroAnimationApp.swift
+//  ProfilePictureHeroAnimation
+//
+//  Created by Zelyna Sillas on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ProfilePictureHeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
